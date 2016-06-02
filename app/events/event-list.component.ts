e16382d497277f47e205f02e8f2e6336252122e1
AlohaCode <bretthooper.maui@gmail.com>
2016-06-02 20:33:29 +0000

    Cleaned cruft

--- app/events/event-list.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from 'angular2/core';
-
-@Component({
-    selector: 'el-events',
-    templateUrl: 'app/events/event-list.component.html'
-})
-export class EventListComponent {
-    pageTitle: string = 'Event List';
-}
